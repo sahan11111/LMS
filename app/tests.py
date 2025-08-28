7185f42758e35235f0535da01f93f4c3f3ce12d5
Sahan Takhachhen <takhachhensahan@gmail.com>
2025-08-28 16:22:52 +0545

    check git push

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Course, Enrollment, Assessment, Submission
 # Create your tests here.
+from django.contrib.auth.models import User, Group
+from rest_framework.test import APIClient           
+from rest_framework import status
+from django.urls import reverse     
+import json
+from rest_framework.authtoken.models import Token
+from core.models import User as CustomUser
+from .serializers import CourseSerializer, EnrollmentSerializer
+from . import models
+from django.utils import timezone
+from datetime import timedelta
