a8d9987efdc244b94cdef15edf4c204e43748346
Sahan Takhachhen <takhachhensahan@gmail.com>
2026-02-27 14:24:04 +0545

    allow host

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "lms-production-733d.up.railway.app",
+    "lms-production-f437.up.railway.app",
     "localhost",
     "127.0.0.1",
 ]
