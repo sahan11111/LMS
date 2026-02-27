b727cfaab06776ed815024f902838dd3dcf727bf
Sahan Takhachhen <takhachhensahan@gmail.com>
2026-02-27 13:01:40 +0545

    add railway in setting '

--- project/settings.py
+++ project/settings.py
@@ -15,13 +15,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
-if not DEBUG:
-    allowed = os.getenv('ALLOWED_HOSTS', '')
-    ALLOWED_HOSTS = [h.strip() for h in allowed.split(',') if h.strip()]
+if DEBUG:
+    ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+else:
+    ALLOWED_HOSTS = [
+        ".railway.app",
+        ".up.railway.app",
+    ]
 
 # Application definition
 INSTALLED_APPS = [
